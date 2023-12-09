973acbb42cef8c7af0a234563a76d3812d0dd04d
Invited11 <118669603+Invited11@users.noreply.github.com>
2023-12-09 15:05:15 +0100

    Lagrage_pas_testee

--- function.c
+++ function.c
@@ -71,3 +71,58 @@ double LeftRightCondition(double x, double y, int cas){
         return 0.0;
   }
 }
+
+
+typedef double* vecteur;
+typedef vecteur* vecteur2D;
+
+//Prend en entrée les points 2D d'interpolation pt_x, les valeurs 2D à matcher pt_y et un point x=(x1,x2) où évaluer ce polynôme et met la valeur du polynôme en ce point dans y=(y1,y2), n le nombre de points d'interpolation
+void LagrangeCoeff(vecteur2D pt_x, vecteur2D pt_y, vecteur x, vecteur y, int n)
+{
+	//Allocations
+	/*x = (double*)malloc(2*sizeof(double));
+	y = (double*)malloc(2*sizeof(double));
+	pt_x = (vecteur*)malloc(n*sizeof(vecteur));
+	pt_y = (vecteur*)malloc(n*sizeof(vecteur));
+	for (int i = 0; i < n; i++)
+	{
+        	pt_x[i] = (double*)malloc(2*sizeof(double));
+        	pt_y[i] = (double*)malloc(2*sizeof(double));
+    	}
+
+	free(x);
+	free(y);*/
+	
+	y[0]=0;
+	y[1]=0;
+	for(int i = 0 ; i < n ; i++)
+	{
+		for(int j = 0 ; j < n ; j++)
+		{
+			double Lx=1;
+		        double Ly=1;
+			for(int k = 0 ; k < n ; k++)
+			{
+				if( k == i && k == j)
+				{
+				}
+				else if(k == i)
+				{
+					Ly *= (x[1] - pt_x[k][1])/(pt_x[j][1] - pt_x[k][1]);
+				}
+				else if(k == j)
+				{
+					Lx *= (x[0] - pt_x[k][0])/(pt_x[i][0] - pt_x[k][0]);
+				}
+				else
+				{
+					Lx *= (x[0] - pt_x[k][0])/(pt_x[i][0] - pt_x[k][0]);
+					Ly *= (x[1] - pt_x[k][1])/(pt_x[j][1] - pt_x[k][1]);
+				}
+			}
+			y[0] += pt_y[i][0] * Lx * Ly;
+			y[1] += pt_y[j][1] * Lx * Ly;
+		}
+	}
+
+}
--- function.h
+++ function.h
@@ -5,4 +5,4 @@ double Source(double x, double y, int cas);
 double UpBottomCondition(double x, double y, int cas);
 double LeftRightCondition(double x, double y, int cas);
 
-void LagrangeCoeff(vecteur2D pt_x, vecteur2D pt_y, vecteur x, vecteur y); //Prend en entrée les points 2D d'interpolation pt_x, les valeurs 2D à matcher pt_y et un point x=(x1,x2) où évaluer ce polynôme et met la valeur du polynôme en ce point dans y=(y1,y2)
+void LagrangeCoeff(vecteur2D pt_x, vecteur2D pt_y, vecteur x, vecteur y, int n); //Prend en entrée les points 2D d'interpolation pt_x, les valeurs 2D à matcher pt_y et un point x=(x1,x2) où évaluer ce polynôme et met la valeur du polynôme en ce point dans y=(y1,y2), n le nb de points
