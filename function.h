5796b00c17c1bf8e7c3d1506d8e9a700b16da506
Invited11 <118669603+Invited11@users.noreply.github.com>
2023-12-09 14:08:51 +0100

    creation_fct

--- function.h
+++ function.h
@@ -1,3 +1,8 @@
+typedef double* vecteur;
+typedef vecteur* vecteur2D;
+
 double Source(double x, double y, int cas);
 double UpBottomCondition(double x, double y, int cas);
 double LeftRightCondition(double x, double y, int cas);
+
+void LagrangeCoeff(vecteur2D pt_x, vecteur2D pt_y, vecteur x, vecteur y); //Prend en entrée les points 2D d'interpolation pt_x, les valeurs 2D à matcher pt_y et un point x=(x1,x2) où évaluer ce polynôme et met la valeur du polynôme en ce point dans y=(y1,y2)
